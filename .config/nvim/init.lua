ee1d215ebb2636ab09766cb0b9c80698d596070f
Jörn Neumeyer <neumeyer@code-lake.com>
2025-06-10 07:21:03 +0200

    add plugin initialization

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -2,7 +2,17 @@
 local vimrc = vim.fn.stdpath("config") .. "/setup.vim"
 vim.cmd.source(vimrc)
 
-
 local lsp_config = require 'lspconfig'
 lsp_config.lua_ls.setup{}
+
+local autoclose = require 'autoclose'
+autoclose.setup()
+
+local pid = vim.fn.getpid()
+
+lsp_config.omnisharp.setup{
+    cmd = { '$HOME/Apps/onmisharp/onmisharp/OmniSharp.exe' , "--languageserver" , "--hostPID", tostring(pid) }
+    -- Additional configuration can be added here
+}
+
 
